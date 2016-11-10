32dc567d8c1c09d1da91502a4960ac2fba2c3fe5
bruce <shulin.bao@transsion.com>
2016-11-10 17:38:44 +0800

    更新一般问题，

--- loginsert/Form1.Designer.cs
+++ loginsert/Form1.Designer.cs
@@ -42,7 +42,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(275, 70);
+            this.button1.Location = new System.Drawing.Point(281, 54);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -52,7 +52,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 44);
+            this.textBox1.Location = new System.Drawing.Point(19, 28);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(259, 21);
             this.textBox1.TabIndex = 1;
@@ -61,7 +61,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(13, 77);
+            this.checkBox1.Location = new System.Drawing.Point(19, 61);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(72, 16);
             this.checkBox1.TabIndex = 2;
@@ -70,14 +70,14 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(54, 122);
+            this.textBox2.Location = new System.Drawing.Point(60, 106);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 21);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(54, 150);
+            this.textBox3.Location = new System.Drawing.Point(60, 134);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 21);
             this.textBox3.TabIndex = 4;
@@ -85,7 +85,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 126);
+            this.label1.Location = new System.Drawing.Point(21, 110);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 12);
             this.label1.TabIndex = 5;
@@ -94,7 +94,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 155);
+            this.label2.Location = new System.Drawing.Point(21, 139);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 12);
             this.label2.TabIndex = 5;
@@ -103,7 +103,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 100);
+            this.label3.Location = new System.Drawing.Point(19, 84);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(239, 12);
             this.label3.TabIndex = 6;
@@ -112,7 +112,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(183, 150);
+            this.label4.Location = new System.Drawing.Point(168, 138);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(167, 12);
             this.label4.TabIndex = 7;
@@ -120,7 +120,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(275, 44);
+            this.button2.Location = new System.Drawing.Point(281, 28);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 0;
--- loginsert/Form1.cs
+++ loginsert/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace loginsert
 {
@@ -29,7 +30,19 @@ namespace loginsert
             {
                 return;
             }
-            content = replaceContent(content);
+            string ext = Path.GetExtension(textBox1.Text);
+
+
+            if (ext.ToLower().EndsWith(".java") == true)
+            {
+                content = JavaReplaceContent(content);
+            }
+            else if (ext.ToLower().EndsWith(".c") == true)
+            {
+                content = JavaReplaceContent(content);
+            }
+            
+
             string filename =  "content"+ DateTime.Now.Ticks.ToString().Substring(7,3)+".txt";
             if (checkBox1.Checked)
             {
@@ -42,9 +55,36 @@ namespace loginsert
             MessageBox.Show("完成");
             
         }
-
-        private string replaceContent(string content)
+        private void EvnInit()
         {
+            if (textBox2.Text.Length > 0)
+            {
+                Tag = textBox2.Text;
+            }
+            else
+            {
+                Tag = "TAG";
+            }
+            if (textBox3.Text.Length > 0)
+            {
+                Name = textBox3.Text;
+            }
+            else
+            {
+                Name = "bruce";
+            }
+        }
+        private string CReplaceContent(string content)
+        {
+            EvnInit();
+            //正则表达  [^ ^.]{1,}\([ ,A-Za-z]*\)[\s]*{
+            MessageBox.Show("尚未开发");
+            return "";
+        }
+
+        private string JavaReplaceContent(string content)
+        {
+            EvnInit();
             //正则表达  [^ ^.]{1,}\([ ,A-Za-z]*\)[\s]*{
             string[] split = content.Split('\n');
 
@@ -66,10 +106,10 @@ namespace loginsert
                 }
                 string tempold = item.Value;
                 string log = "\r\n\tLog.d(" + Tag + ", \""+Name+" " + item.Groups["fun"] + "() + index " + index + "\");\r\n";
-                string tempnew = item.Value + log;
                 index++;
-                if( CheckContentItem(tempold,split))
+                if( CheckContentItem(ref tempold,split))
                 {
+                    string tempnew = tempold + log;
                     content = content.Replace(tempold, tempnew);
                 }
                 
@@ -80,12 +120,22 @@ namespace loginsert
             return content;
         }
 
-        private bool CheckContentItem(string tempold, string[] split)
+        private bool CheckContentItem(ref string tempold, string[] split)
         {
+            int  i = 0;
             foreach (string item in split)
             {
+                i++;
                 if (item.Contains(tempold) && item.Contains("new ") == false)
                 {
+                    if (i < split.Length && split[i].Contains("super("))
+                    {
+                        tempold = split[i]; //改变old字符串，在下一行添加log
+                    }
+                    if (i < split.Length && split[i-1].Trim().StartsWith("//"))
+                    {
+                        return false;
+                    }
                     return true;
                 }
             }
